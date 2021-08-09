973870244c586a11dce93ce5349c0d7a88d48679
Ahmed <ahmedali999>
2021-08-09 17:22:40 +0200

    python code

--- Task2/robot_ws/src/counter/script/publisher.py
+++ Task2/robot_ws/src/counter/script/publisher.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python 3.8
 
 import rospy
 from std_msgs.msg import int16
 
 def talker():
     pub= rospy.Publisher('topic', int,queue_size=10)
-    rospy.init_node('node1')
-    rate = rospy.Rate(2)
-
+    rospy.init_node('node1', anonymous=True)
+    rate = rospy.Rate(5)
+    counter = 0
     while not rospy.is_shutdown():
     
-     counter = counter + 1   
+        counter = counter + 1   
         rospy.loginfo (counter)
         pub.publish(counter)
         rate.sleep()
 
-      talker()
+talker()
--- Task2/robot_ws/src/counter/script/subscriber.py
+++ Task2/robot_ws/src/counter/script/subscriber.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python  3.8
 
 import rospy
 from std_msgs.msg import int16
@@ -7,8 +7,8 @@ def call (msg):
     rodpy.loginfo(rospy.get_caller_id() + "I heard %s", msg.data)
 
 def listener ():
-    rospy,init_node('listener', anonymous=True)
-    rospy.Subscriber('counter', int16,call)
+    rospy,init_node("node2", anonymous=True)
+    rospy.Subscriber('topic', int16,call)
     rospy,spin ()
 
-    listener()
+listener()
